1faa6ca912557cd41e6b9c9520be656b8aedc6a5
Angelo <>
2019-04-25 15:53:59 +0200

    Added a template

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"log"
+
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoList struct {
+	Title string
+	Todos []Todo
+}
+
 func main() {
 	log.Println("Booting up webserver...")
+	tmpl := template.Must(template.ParseFiles("layout.html"))
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "You've requested on URL: %s\n", r.URL.Path)
+		// fmt.Fprintf(w, "You've requested on URL: %s\n", r.URL.Path)
+		data := TodoList{
+			Title: "My todo list",
+			Todos: []Todo{
+				{"Book 1", true},
+				{"Book 2", true},
+				{"Book 3", false},
+			},
+		}
+		tmpl.Execute(w, data)
 	})
 
-	r.Handle("/files/", http.FileServer(http.Dir("static/")))
+	// blogR := r.PathPrefix("/blog/").Subrouter()
+
+	// blogR.HandleFunc("/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	// 	page := mux.Vars(r)["page"]
+	// 	fmt.Fprintf(w, "This is page %s of the blog", page)
+	// })
 
 	log.Println("Booted up webserver")
 	http.ListenAndServe(":80", r)
